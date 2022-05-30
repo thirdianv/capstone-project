82ce0d6af95b5094597509b6fb7b77748d0261f3
isan0880 <isanaditya33@gmail.com>
2022-05-30 13:46:39 +0700

    tampilan halaman 1 dan 2

---
+++ app/src/main/java/com/bangkit2022/bentala/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit2022.bentala.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit2022.bentala.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/bangkit2022/bentala/view/StartActivity.kt
@@ -0,0 +1,17 @@
+package com.bangkit2022.bentala.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.bangkit2022.bentala.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+
+    }
+
+}
+
