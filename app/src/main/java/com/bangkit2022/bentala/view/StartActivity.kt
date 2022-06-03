9ebdfcafb891fcaef4740d30db2b91efa820ddf8
isan0880 <isanaditya33@gmail.com>
2022-06-03 22:19:24 +0700

    add text view hasil

--- app/src/main/java/com/bangkit2022/bentala/view/StartActivity.kt
+++ app/src/main/java/com/bangkit2022/bentala/view/StartActivity.kt
@@ -1,15 +1,30 @@
 package com.bangkit2022.bentala.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import android.widget.Button
 import com.bangkit2022.bentala.R
 
 class StartActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
+        val button =findViewById<Button>(R.id.tv_button_arrow_forward)
 
+        button.setOnClickListener {
+            callActivity()
+        }
+
+    }
+    private fun callActivity(){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
 }
