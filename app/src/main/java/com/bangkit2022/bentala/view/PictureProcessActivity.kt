cc686fdafb4134adf7aee28209126995ff0421a1
aryapraset <aryaprasetiyo12@gmail.com>
2022-06-12 13:53:02 +0800

    Adding some code

--- app/src/main/java/com/bangkit2022/bentala/view/PictureProcessActivity.kt
+++ app/src/main/java/com/bangkit2022/bentala/view/PictureProcessActivity.kt
@@ -10,10 +10,6 @@ import android.graphics.ColorSpace.Model
 import android.media.ThumbnailUtils
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -22,20 +18,42 @@ import androidx.core.content.ContextCompat
 import com.bangkit2022.bentala.databinding.ActivityPictureProcessBinding
 import com.bangkit2022.bentala.ml.KlasifikasiJenisTanah3
 import org.tensorflow.lite.DataType
-import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.*
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
 
 class PictureProcessActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPictureProcessBinding
-    private lateinit var imageView: ImageView
-    private lateinit var buttonCamera: Button
-    private lateinit var buttonLoadImage: Button
-    private lateinit var tvResult: TextView
-    private val GALLERY_REQUEST_CODE = 123
+
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,45 +61,98 @@ class PictureProcessActivity : AppCompatActivity() {
         binding = ActivityPictureProcessBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        imageView = binding.previewImageView
-        buttonCamera = binding.btnCameraXButton
-        tvResult = binding.tvTextHasil
-        buttonLoadImage = binding.galleryButton
-
-        buttonCamera.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED
-            ) {
-                takePicturePreview.launch(null)
-            }
-            else {
-                requestPermission.launch(android.Manifest.permission.CAMERA)
-            }
-        }
-        buttonLoadImage.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED
-            ){
-                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                intent.type = "image/*"
-                val mimeTypes = arrayOf("image/jpeg","image/png","image/jpg")
-                intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
-                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-                onresult.launch(intent)
-            }else {
-                requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-            }
-        }
-        tvResult.setOnClickListener{
-            val intent = Intent(Intent.ACTION_VIEW, {tvResult})
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
         }
 
+        binding.btnCameraXButton.setOnClickListener { startCameraX() }
+        binding.galleryButton.setOnClickListener { startGallery() }
 
-
-
-
-
+        binding.btnHelp.setOnClickListener { callHelpPage() }
+        binding.btnBack.setOnClickListener{ callBackMainActivity() }
+    }
+    private fun callHelpPage(){
+        startActivity(Intent(this, HelpActivity::class.java))
+        finish()
+    }
+    private fun callBackMainActivity(){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
     }
 
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun startTakePhoto() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+
+            binding.previewImageView.setImageBitmap(result)
+
+        }
+    }
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@PictureProcessActivity)
+            binding.previewImageView.setImageURI(selectedImg)
+        }
+    }
+
+    fun classifyImage(image: Bitmap) {
+        val model = KlasifikasiJenisTanah3.newInstance(applicationContext)
+
+// Creates inputs for reference.
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 150, 150, 3), DataType.FLOAT32)
+        inputFeature0.loadBuffer(byteBuffer)
+
+// Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+// Releases model resources if no longer used.
+        model.close()
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            var image = data!!.extras!!["data"] as Bitmap?
+            val dimension = Math.min(image!!.width, image.height)
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension)
+            imageView.setImageBitmap(image)
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
+            classifyImage(image)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 
 }
